4ad6c6801c053be505ad9c5d3157f3bfc09b420c
Prathi Koteswara Rao <98726537+kotiprathi@users.noreply.github.com>
2023-11-21 15:01:48 +0530

    Update streamlit_app.py
    errors fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,17 +39,6 @@ except URLError as e:
   streamlit.error()
 streamlit.write('The user entered', fruit_choice)
 
-'''
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) # just writes the data to the screen
-
-# take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-'''
-
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
